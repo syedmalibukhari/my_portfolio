43c3780a36537086a7fd6494ffe4b58edab5aced
Syed mohammad Ali Bukhari <157492665+syedmalibukhari@users.noreply.github.com>
2024-03-03 16:00:38 +0000

    pictures added underconstruction yet

--- scripts.js
+++ scripts.js
@@ -35,17 +35,5 @@ let changeText = () => {
 changeText();
 setInterval(changeText, 3000);
 
-// circle skill /////////////////////////////////////
-const circles = document.querySelectorAll(".circle");
-circles.forEach(elem =>{
-    var dots = elem.getAttribute("data-dots");
-    var marked = elem.getAttribute("data-percent");
-    var percent = Match.floor(dots*marked/100);
-    var points = "";
-    var rotate = 360 / dots;
-
-    for (let i = 0; i < dots; i++){
-        points += '<div class="points" style="--i:${i}; --rot${rotate}deg"></div>'
-    }
-    elem.innerHTML = points;
-});
+// portfolio gallery /////////////////////////////////////
+var mixer = mixitup('.portfolio-gallery');
