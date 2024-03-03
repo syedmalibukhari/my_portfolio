7a2157bc601b5ee10001c8802e5d074b596b4e1c
Syed mohammad Ali Bukhari <157492665+syedmalibukhari@users.noreply.github.com>
2024-03-03 22:33:46 +0000

    updating

--- scripts.js
+++ scripts.js
@@ -37,3 +37,31 @@ setInterval(changeText, 3000);
 
 // portfolio gallery /////////////////////////////////////
 var mixer = mixitup('.portfolio-gallery');
+
+
+// active menu /////////////////////////////////////
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){};
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll",activeMenu);
+
+
+
+// sticky menu /////////////////////////////////////
+const header = document.querySelector("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",window.scrollY > 50);
+});
+
+
+
+
+
